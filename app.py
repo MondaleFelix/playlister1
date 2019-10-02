4a2c98476288845bd1e1d5673d1d8851730f8275
MondaleFelix <mondalefelix@yahoo.com>
2019-10-01 21:20:59 -0700

    fixing

--- app.py
+++ app.py
@@ -3,14 +3,10 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
+
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-playlists = db.playlists
-
-
-client = MongoClient()
-db = client.Playlister
 playlists = db.playlists
 
 app = Flask(__name__)
