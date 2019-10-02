6bff88c628043fcdd6a9e636c08929daf3be6d8e
MondaleFelix <mondalefelix@yahoo.com>
2019-10-01 18:14:16 -0700

    Users can see all playlists

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	# Return Homepage
-	return render_template('home.html', msg="Flask is cool")
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
 
+@app.route("/")
+def playlists_index():
+	# Show all playlists
+	return render_template("playlists_index.html", playlists = playlists)
 
 
 if __name__ == '__main__':
